08d5664d822dc41fddd3fec6c050341ab60259bf
Umut Berk Bilgiç <umutbilgicapple@gmail.com>
2019-05-02 12:32:41 +0300

    Add language detection based stop word removal

--- lyric_preprocessor.py
+++ lyric_preprocessor.py
@@ -11,6 +11,7 @@ import csv
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords 
 import re
+from langdetect import detect
 
 print("Lyric pre-processor start ...")
 
@@ -22,14 +23,9 @@ count = 0
 # Stemmer initialize
 porter = PorterStemmer()
 
-# Stop words initialize
-stop_words = set(stopwords.words('english')) 
-stop_words.add("you")
-stop_words.add("I")
-stop_words.add("im")
-stop_words.add("I'm")
-stop_words.add("i'm")
-stop_words.add("i")
+# milestone count
+mlc = 0
+milestone = 20000
 
 # Open csv file and fill dataset list
 print("  ! Start preprocessing of dataset ...")
@@ -37,9 +33,32 @@ with open("lyrics.csv", "rt", encoding="utf8") as csvfile:
     wsreader = csv.reader(csvfile, delimiter=',')
     for row in wsreader:
         if flag:
-            # Lower case -> replace new llines with space -> remove special characters
-            lyric = porter.stem(((row[5].lower())).replace("\n", " ").translate({ord(i): None for i in "#%&/*\"!?.\',"}))
-                                 
+            # Lower case -> replace new lines with space -> remove special characters -> stem
+            lyric = porter.stem(((row[5].lower())).replace("\n", " ").translate({ord(i): None for i in "-_():#%&/*\"!?.\',"}))
+                
+            # language detection
+            try:
+                lang = detect(lyric)
+            except:
+                continue                
+                
+            stop_lang = "english"
+            
+            if (lang == "es"):
+                stop_lang = "spanish"
+            elif (lang == "it"):
+                stop_lang = "italian"
+            elif (lang == "fr"):
+                stop_lang = "french"
+            elif (lang == "de"):
+                stop_lang = "german"
+                
+            stop_words = set(stopwords.words(stop_lang))
+            if (stop_lang == "english"):
+                stop_words.add("im")
+                stop_words.add("i'm")
+                stop_words.add("i am")
+            
             # Tokenize on space characters and remove stop words
             lyric_tokens = lyric.split(" ")            
             
@@ -51,17 +70,21 @@ with open("lyrics.csv", "rt", encoding="utf8") as csvfile:
             filtered_lyric = " ".join(filtered_lyric) # Join array of strings with space
             row[5] = re.sub(' +', ' ', filtered_lyric) # Remove multiple occurances of space with regex
             dataset.append(row)
+            if(mlc % milestone == 0):
+                print("Milestone reach: " + str(mlc / milestone))
+            mlc += 1
+            
         else:
             flag = True 
             
 # Clean up for helping the csv process below
-del flag, row, count, filtered_lyric, lyric, lyric_tokens, 
+del flag, row, count, filtered_lyric, lyric, lyric_tokens, mlc, milestone
 
 print("  ! Completed preprocessed dataset generation.")
 print("  ! Writing to .csv file ...")
 
 # Write the 2D dataset to a new .csv file
-with open("lyrics_preproc.csv", "w+", encoding="utf8") as new_csv:
+with open("lyrics_preproc.csv", 'w+', newline='', encoding="utf8") as new_csv:
     csvWriter = csv.writer(new_csv, delimiter=',')
     csvWriter.writerows(dataset)
     
@@ -71,4 +94,3 @@ print("End of lyric pre-processor")
 
 
     
-
